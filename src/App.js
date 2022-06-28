d1cb1870903e2d1f3e2d74f289518b9feaeb5c53
sojeongsojeong-park <psj90972@naver.com>
2022-06-28 22:48:45 +0900

    feat:ExpenseItem 구현

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Let's get started!</h1>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
