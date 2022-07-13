8d9f6e5b739f374145f6283a2c4b3740a342e88d
sojeongsojeong-park <psj90972@naver.com>
2022-07-14 00:39:41 +0900

    feat: conditional rendering

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,11 +5,6 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // const [title, setTitle] = useState(props.title);
-
-  // const clickHandler = () => {
-  //   setTitle("updated!")
-  // }
 
   return (
     <Card className='expense-item'>
