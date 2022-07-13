12f9c964a6cae29dadbf87a625f632972b7ab27c
sojeongsojeong-park <psj90972@naver.com>
2022-07-13 13:14:16 +0900

    feat: implement conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,9 +10,20 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  const filteredExpense = props.items.filter(expense => {
+  const filteredExpense = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  let expensesContent = <p>No expenses found.</p>;
+  if(filteredExpense.length > 0){
+    expensesContent = filteredExpense.map((expense) => (
+      <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+    ))
+  }
   return (
     <div>
       <Card className='expenses'>
@@ -20,14 +31,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpense.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
