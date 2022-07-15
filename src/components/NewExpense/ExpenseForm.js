fb9eb7c96800f5f9aa3e4a30a4e53897014e67d3
sojeongsojeong-park <psj90972@naver.com>
2022-07-15 20:29:51 +0900

    feat: implement editing

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,6 +50,8 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
+
+
   return (
     <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
@@ -83,6 +85,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -9,15 +9,19 @@ const NewExpense = (props) => {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    props.onAddExpense(expenseData)
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
   }
   const startEditingHandler = () => {
     setIsEditing(true)
   }
+  const stopEditingHandler = () => {
+    setIsEditing(false)
+  }
   return (
       <div className="new-expense">
         {!isEditing && <button onClick={startEditingHandler}>Add NewExpense</button>}
-        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
       </div>
   )
 }
