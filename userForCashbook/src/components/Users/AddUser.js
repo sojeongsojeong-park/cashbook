23445e3c7bd5d81a7df5a1af56b6c6d803aacae2
sojeongsojeong-park <psj90972@naver.com>
2022-09-01 09:21:51 +0900

    create userForCashbook

---
+++ userForCashbook/src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/Card";
+
+function AddUser() {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' />
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
+  );
+}
+
+export default AddUser;
