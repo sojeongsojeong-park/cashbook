dbc94912551f0c9fcd9008ae6ddda704d344a925
sojeongsojeong-park <psj90972@naver.com>
2022-09-02 13:39:47 +0900

    feat: add Card style

--- userForCashbook/src/components/Users/AddUser.js
+++ userForCashbook/src/components/Users/AddUser.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Card from "../UI/Card";
 
+import classes from "./AddUser.module.css";
+
 function AddUser() {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
         <input id='username' type='text' />
