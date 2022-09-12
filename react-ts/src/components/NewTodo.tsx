55512591b7fa5f2a9627edb55c3bc855b4b7c6c7
sojeongsojeong-park <psj90972@naver.com>
2022-09-12 10:28:52 +0900

    style: add style

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 
+import classes from './NewTodo.module.css'
+
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
   const submitHandler = (e: React.FormEvent) => {
@@ -14,7 +16,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     props.onAddTodo(enteredText);
   };
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor='text'>Todo Text</label>
       <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
