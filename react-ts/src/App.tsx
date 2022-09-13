a21298d7ea9dd2252ea999d477581535371b694a
sojeongsojeong-park <psj90972@naver.com>
2022-09-13 12:20:25 +0900

    feat: implement deleting todo function by clicking each todo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,19 +4,24 @@ import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (text: string) => {
     const newTodo = new Todo(text);
 
     setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo)
-    })
+      return prevTodos.concat(newTodo);
+    });
+  };
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
   };
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
