f0ec3c36ad0ba4b55f66d0a1b8089c79248137c5
sojeongsojeong-park <psj90972@naver.com>
2022-09-15 15:02:39 +0900

    feat: implement debouncing using useEffect

--- studyReducer/src/components/Login/Login.js
+++ studyReducer/src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    //clean up
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
