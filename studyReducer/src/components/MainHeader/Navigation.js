d1fe1ffea4fc50762ae712a301e225a6b1bacf00
sojeongsojeong-park <psj90972@naver.com>
2022-09-27 12:36:41 +0900

    refactor: refactor using contextAPI

--- studyReducer/src/components/MainHeader/Navigation.js
+++ studyReducer/src/components/MainHeader/Navigation.js
@@ -4,29 +4,29 @@ import AuthContext from "../../store/auth-context";
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
-  const ctx = useContext(AuthContext)
+  const ctx = useContext(AuthContext);
 
   return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
