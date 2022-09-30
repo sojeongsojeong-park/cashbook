2247d67c282a27e85fe2c2b780b419ac576ca391
sojeongsojeong-park <psj90972@naver.com>
2022-09-30 12:43:53 +0900

    feat: implement cart button in header

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import mealsImage from "../../assets/meals.jpeg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>Foodeli</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt='A table full of delicious food!' />
