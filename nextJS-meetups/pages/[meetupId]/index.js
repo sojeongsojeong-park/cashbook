7f97429a9b15dab107f0c1c680b30b0f792d5382
sojeongsojeong-park <psj90972@naver.com>
2022-10-05 11:13:59 +0900

    feat: implement dynamic routing for meetup detail

---
+++ nextJS-meetups/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image='https://cdn.pixabay.com/photo/2017/01/12/02/21/melbourne-1973533_1280.jpg'
+      title='First Meetup'
+      address='Some address 4, 12342 melbourne'
+      description='This is a first meetup description!'
+    />
+  );
+};
+
+export default MeetupDetails;
