c55b0fa059ad4b382fcb27904e0f0909a6937792
sojeongsojeong-park <psj90972@naver.com>
2022-10-06 23:18:35 +0900

    feat: implement getStaticProps function

--- nextJS-meetups/pages/index.js
+++ nextJS-meetups/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -27,8 +28,16 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
