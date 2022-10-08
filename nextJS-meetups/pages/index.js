0bce8f683aa3c5aa6338044dc4fb72752d551b49
sojeongsojeong-park <psj90972@naver.com>
2022-10-08 10:01:57 +0900

    feat: implement get data function from mongodb

--- nextJS-meetups/pages/index.js
+++ nextJS-meetups/pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -32,9 +33,21 @@ const HomePage = (props) => {
 };
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://sojeong:12341234@cluster0.ujbdnkg.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 3600,
   };
