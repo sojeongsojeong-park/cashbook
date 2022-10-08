6660d18a1c838340a5f9fbc0e573d8d40b78d953
sojeongsojeong-park <psj90972@naver.com>
2022-10-08 12:14:30 +0900

    feat:implement get data function from mongodb

--- nextJS-meetups/pages/index.js
+++ nextJS-meetups/pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -33,11 +33,33 @@ const HomePage = (props) => {
 };
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://sojeong:${process.env.NEXT_PUBLIC_PASSWORD}@cluster0.ujbdnkg.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
+    revalidate: 3600,
   };
 }
+// export async function getServerSideProps(context){
+//   const req = context.req;
+//   const res = context.res
+//   return {
+//     props:{
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
 
 export default HomePage;
