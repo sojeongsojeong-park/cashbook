4b506b13cfdac0c61fa2f1f285b908410144de25
sojeongsojeong-park <psj90972@naver.com>
2022-10-22 18:11:03 +0900

    feat: implement data post method using custom hook

--- task-app/src/components/NewTask/TaskForm.js
+++ task-app/src/components/NewTask/TaskForm.js
@@ -1,24 +1,25 @@
-import { useRef } from 'react';
+import { useState } from "react";
 
-import classes from './TaskForm.module.css';
+import classes from "./TaskForm.module.css";
 
 const TaskForm = (props) => {
-  const taskInputRef = useRef();
+  const [enteredValue, setEnteredValue] = useState("");
 
+  const inputHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
   const submitHandler = (event) => {
     event.preventDefault();
-
-    const enteredValue = taskInputRef.current.value;
-
     if (enteredValue.trim().length > 0) {
       props.onEnterTask(enteredValue);
+      setEnteredValue("");
     }
   };
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
-      <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
+      <input type='text' onChange={inputHandler} value={enteredValue} />
+      <button>{props.loading ? "Sending..." : "Add Task"}</button>
     </form>
   );
 };
