422df2923b9869eec28fb47e6e1327a1e60e1c59
sojeongsojeong-park <psj90972@naver.com>
2022-11-10 16:33:25 +0900

    feat: study redux basic

---
+++ redux/reduxInJS/redux-demo.js
@@ -0,0 +1,30 @@
+const redux = require("redux");
+
+const couterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(couterReducer);
+
+console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
