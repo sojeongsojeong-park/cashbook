3d78d97e7b235239afe6b2290786edf9bed5137d
sojeongsojeong-park <psj90972@naver.com>
2022-11-18 23:00:17 +0900

    feat: working with literal type

--- udemy-TypeScript/app.ts
+++ udemy-TypeScript/app.ts
@@ -1,14 +1,27 @@
-function combine(n1: string | number, n2: string | number) {
+function combine(
+  n1: string | number,
+  n2: string | number,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    result = n1 + n2;
+  if (
+    (typeof n1 === "number" && typeof n2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
-
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
   return result;
 }
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-const combinedNames = combine("sojeong", "park");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("sojeong", "park", "as-text");
 console.log(combinedNames);
