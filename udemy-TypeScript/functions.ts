59bc9379f022c91fd61ad24752201f3d585dac57
sojeongsojeong-park <psj90972@naver.com>
2022-11-19 10:42:13 +0900

    feat: working with function type

---
+++ udemy-TypeScript/functions.ts
@@ -0,0 +1,26 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(7, 8));
+
+//let someValue: undefined; 유효한 타입임!
+
+addAndHandle(4, 5, (result) => {
+  console.log(result);
+});
