057e076b41dc7bee3d138737cf534e4e9c762669
sojeongsojeong-park <psj90972@naver.com>
2022-11-19 16:21:23 +0900

    feat: study unknown & never type

---
+++ udemy-TypeScript/app.ts
@@ -0,0 +1,15 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "sojeong";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+const result = generateError("An error occured", 500);
+console.log(result);
