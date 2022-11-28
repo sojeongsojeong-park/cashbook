cb9b76e1b44d1bcc209fd2b115a25c35e828bf23
sojeongsojeong-park <psj90972@naver.com>
2022-11-28 22:42:33 +0900

    feat: implement add todo & delete todo

--- todo-with-typescript/src/App.tsx
+++ todo-with-typescript/src/App.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
+  };
+  const todoDeleteHandler = (todoId:string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
+  }
+
+  return (
+    <div className='App'>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
 };
 
 export default App;
---
+++ todo-with-typescript/src/components/NewTodo.tsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInput = useRef<HTMLInputElement>(null);
+  const todoSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredText = textInput.current!.value;
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor='todo-text'>Todo Text</label>
+        <input type='text' id='todo-text' ref={textInput} />
+      </div>
+      <button type='submit'>ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
