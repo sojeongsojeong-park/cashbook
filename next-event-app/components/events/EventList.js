581edd3abe1f99e51dc0efd596480799d57f0b47
sojeongsojeong-park <psj90972@naver.com>
2022-12-11 14:18:54 +0900

    feat: implement evnet list

---
+++ next-event-app/components/events/EventList.js
@@ -0,0 +1,23 @@
+import EventItem from "./EventItem";
+
+const EventList = (props) => {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => {
+        return (
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            location={event.location}
+            date={event.date}
+            image={event.image}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default EventList;
