c557862b4fffa30399570682d7cb571046f583f1
sojeongsojeong-park <psj90972@naver.com>
2022-12-17 22:08:21 +0900

    feat: implement events page ui

--- next-event-app/pages/events/index.js
+++ next-event-app/pages/events/index.js
@@ -1,8 +1,15 @@
+import { Fragment } from "react";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/Events-search";
+import { getAllEvents } from "../../dummy-data";
+
 function AllEventsPage() {
+  const events = getAllEvents();
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
