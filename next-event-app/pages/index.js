bc47af43f9df6b9a640043df47e303f93d45f630
sojeongsojeong-park <psj90972@naver.com>
2022-12-24 16:20:27 +0900

    feat: move dummy-data to firebase

--- next-event-app/pages/index.js
+++ next-event-app/pages/index.js
@@ -1,16 +1,25 @@
 import EventList from "../components/events/EventList";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { events } = props;
 
   return (
     <div>
       <ul>
-        <EventList items={featuredEvents} />
+        <EventList items={events} />
       </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
